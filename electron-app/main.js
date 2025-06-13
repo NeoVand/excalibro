660cb9b8fafac4d644c5327aa83ce13abc3262d0
Neo Mohsenvand <mmv@mit.edu>
2025-06-12 22:49:16 -0500

    Adjust electron build to bundle renderer

--- electron-app/main.js
+++ electron-app/main.js
@@ -16,7 +16,7 @@ function createWindow() {
   if (isDev) {
     win.loadURL("http://localhost:3000");
   } else {
-    win.loadFile(path.join(__dirname, "../excalidraw-app/build/index.html"));
+    win.loadFile(path.join(__dirname, "renderer/index.html"));
   }
 }
 
